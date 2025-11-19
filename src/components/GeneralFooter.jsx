754755380e123e4e8f8841987090eccb55d838ba
Ayoola <ayoolatijesuni2017@gmail.com>
2025-11-19 22:06:56 +0100

    deku

--- src/components/GeneralFooter.jsx
+++ src/components/GeneralFooter.jsx
@@ -1,8 +1,86 @@
+import { Link } from "react-router-dom";
+
 const GeneralFooter = () => {
   return (
-    <div className="bg-black p-10">
-      <div className="container text-white">
-        <div className="text-center">Copyright 2025. All rights reserved</div>
+    <div className="flex flex-col bg-black text-[#626262] text-[14px] font-semibold px-32">
+      <img src="/public/images/logo-full-white.svg" alt="logo-full-white" className="w-32  py-[50px]"/>
+      <div className="flex flex-wrap">
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Personal</h1>
+          <Link>Buy Crypto</Link>
+          <Link>Sell Crypto</Link>
+          <Link>Swap Crypto</Link>
+          <Link>Learn about Crypto</Link>
+          <Link>Crypto Price</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Business</h1>
+          <Link>On Ramps</Link>
+          <Link>Off Ramps</Link>
+          <div><Link>Virtual Accounts </Link><span>New!</span></div>
+          <Link>MoonPay Discover</Link>
+          <Link>MoonPay Commerce</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Company</h1>
+          <Link>About Us</Link>
+          <div><Link>Careers </Link><span>We're hiring</span></div>
+          <Link>Newsroom</Link>
+          <Link>Media</Link>
+          <Link>Changelog</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Support</h1>
+          <Link>API Docs</Link>
+          <Link>Help Center</Link>
+          <Link>Contact Us</Link>
+          <Link>Status</Link>
+          <Link>Security</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Learn</h1>
+          <Link>What is Blockchain?</Link>
+          <Link>What are NFTs?</Link>
+          <Link>What is Bitcoin mining?</Link>
+          <Link>What is DeFi?</Link>
+          <Link>What is crypto staking?</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Buy</h1>
+          <Link>Buy Bitcoin</Link>
+          <Link>Buy Ethereum</Link>
+          <Link>Buy Solana</Link>
+          <Link>Buy Cardano</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Sell</h1>
+          <Link>Sell Bitcoin</Link>
+          <Link>Sell Ethereum</Link>
+          <Link>Sell XRP</Link>
+          <Link>Sell Solana</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Swap</h1>
+          <Link>Swap Ethereum</Link>
+          <Link>Swap SOL</Link>
+          <Link>Swap Aave</Link>
+          <Link>Swap Chainlink</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[25%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Crypto Prices</h1>
+          <Link>Bitcoin Price</Link>
+          <Link>Ethereum Price</Link>
+          <Link>Dogecoin Price</Link>
+          <Link>XRP Price</Link>
+          <Link>Cardano Price</Link>
+        </div>
+        <div className="flex flex-col gap-3 w-[20%] mb-7">
+          <h1 className="text-white mb-3 text-[16px]">Legal</h1>
+          <Link>Licenses</Link>
+          <Link>Privacy Policy</Link>
+          <Link>Cookie Policy</Link>
+          <Link>Terms of Use</Link>
+        </div>        
       </div>
     </div>
   );
--- src/components/GeneralHeader.jsx
+++ src/components/GeneralHeader.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { LuSquareMenu } from "react-icons/lu";
+import { CgMenuGridO } from "react-icons/cg";
+import { CiGlobe } from "react-icons/ci";
 
 const GeneralHeader = () => {
   return (
-    <div className="w-screen py-5 bg-[#F9F8FB] shadow-sm ">
+    <div className="w-screen py-5 px-28 bg-[#F9F8FB] shadow-sm ">
       {/* Desktop navbar */}
       <div className="container flex items-center justify-between">
         {/* Left side */}
@@ -23,8 +24,16 @@ const GeneralHeader = () => {
             <Link to="">Buy</Link>
             <Link to="">Sell</Link>
             <Link to="">Swap</Link>
-            <LuSquareMenu />
+            <div className="flex items-center relative w-8 h-9 ">
+              <CgMenuGridO size={25}/>
+              <div className="bg-[#9A38FF] rounded-full w-2.5 h-2.5 absolute top-0 right-0"></div>
+            </div>
           </div>
+        </div>
+        {/* rightside */}
+        <div className="flex justify-between items-center font-bold gap-7">
+          <CiGlobe size={20} strokeWidth={1} />
+          <button className="bg-black rounded-lg px-5 py-2 text-white">Get Started</button>
         </div>
       </div>
     </div>
